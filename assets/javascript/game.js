defee412404891c15ac12ebd8991d45b97804db3
Ryan <rfoss28@comcast.net>
2018-06-30 19:00:14 -0400

    commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    //Initialize variables
+    var blueCrystalRate=0;
+    var greenCrystalRate=0;
+    var redCrystalRate=0;
+    var yellowCrystalRate=0;
+    var computerTotal=0;
+    var playerTotal=0;
+
+
+    $(".")
+
+});
