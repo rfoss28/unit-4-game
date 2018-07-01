cb28e88df415d2eff1e497901ef0f76c8bf20bf2
Ryan <rfoss28@comcast.net>
2018-07-01 11:37:16 -0400

    Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Initialize variables
     var blueCrystalRate = 0;
@@ -12,56 +12,63 @@ $(document).ready(function() {
 
     initializeGame();
 
-     //Assigns random values to the crystals and computer total
-    function initializeGame(){
-       
-        blueCrystalRate=Math.floor(Math.random() * 12) + 1;
-        greenCrystalRate=Math.floor(Math.random() * 12) + 1;
-        redCrystalRate=Math.floor(Math.random() * 12) + 1;
-        yellowCrystalRate=Math.floor(Math.random() * 12) + 1;
-        computerTotal=Math.floor(Math.random() * 100) + 21;
-        playerTotal=0;
+    //Assigns random values to the crystals and computer total
+    function initializeGame() {
+
+        blueCrystalRate = Math.floor(Math.random() * 12) + 1;
+        greenCrystalRate = Math.floor(Math.random() * 12) + 1;
+        redCrystalRate = Math.floor(Math.random() * 12) + 1;
+        yellowCrystalRate = Math.floor(Math.random() * 12) + 1;
+        computerTotal = Math.floor(Math.random() * 100) + 21;
+        playerTotal = 0;
         $("#computer-score").text(computerTotal);
-        $("#wins").text("Wins: " + wins);
-        $("#losses").text("Losses: " + losses);
-        
+       
+
     }
 
     //checks to see if the player's total is greater than the computer's total
-    function checkScore(){
+    function checkScore() {
         console.log("CheckScore");
-        if(playerTotal>computerTotal){
-           losses++;
-           initializeGame();
-        }
-        else if (playerTotal===computerTotal) {
-            wins++;
+        if (playerTotal > computerTotal) {
+            losses++;
+            $("#losses").text("You Lost! losses: " + losses);
+           
             initializeGame();
         }
-    
+        else if (playerTotal === computerTotal) {
+            wins++;
+            $("#wins").text("You Won! Wins: " + wins);
+            initializeGame();
+        }
+        else{
+            $("#wins").text("Wins: " + wins);
+            $("#losses").text("Losses: " + losses);
+
+        }
+
     }
 
-    $("#blueCrystal").on("click", function() {
-        playerTotal+=blueCrystalRate;
+    $("#blueCrystal").on("click", function () {
+        playerTotal += blueCrystalRate;
         $("#player-score").text(playerTotal);
         checkScore();
 
     });
 
-    $("#greenCrystal").on("click", function() {
-        playerTotal+=greenCrystalRate;
+    $("#greenCrystal").on("click", function () {
+        playerTotal += greenCrystalRate;
         $("#player-score").text(playerTotal);
         checkScore();
 
     });
-    $("#redCrystal").on("click", function() {
-        playerTotal+=redCrystalRate;
+    $("#redCrystal").on("click", function () {
+        playerTotal += redCrystalRate;
         $("#player-score").text(playerTotal);
         checkScore();
 
     });
-    $("#yellowCrystal").on("click", function() {
-        playerTotal+=yellowCrystalRate;
+    $("#yellowCrystal").on("click", function () {
+        playerTotal += yellowCrystalRate;
         $("#player-score").text(playerTotal);
         checkScore();
 
